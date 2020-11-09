9acc6d935c2b8836e7833bbf941d8af9183ea218
Taylor Booth <taylorkbooth@gmail.com>
2020-11-08 21:55:09 -0500

    commit tree 8e417152cd27b069ef9b5adbd8f6969b4b3f04d3

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+require 'pry'
+class School
+
+def initialize(name)
+    @name = name
+    @roster = {}
+end
+
+def roster
+    @roster
+end
+
+def add_student(name, grade)
+    if(roster[grade])
+        roster[grade] << name
+    else 
+        roster[grade] = []
+        roster[grade] << name
+    end
+    roster
+end
+    
+
+def grade(grade)
+    roster[grade]
+end
+
+def sort 
+    roster.each do |key, value|
+        value.sort!
+    end
+    roster.sort.to_h
+end
+
+# binding.pry
+end
